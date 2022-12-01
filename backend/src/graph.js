df6da2754130c10c8c0cc15931de05168240c2ec
faco400 <vd.souza1999@gmail.com>
2022-12-01 18:48:33 -0300

    Add shortest distance helper and fix database error

--- backend/src/graph.js
+++ backend/src/graph.js
@@ -1,7 +1,21 @@
 const {cities} = require('./database/states') 
 
-function testDB(){
-  var myJsonString = JSON.stringify(cities)
-  console.log(myJsonString)
+// search short distance between nodes
+let shortestDistanceNode = (distances, visited) => {
+  let shortest = null;
+
+  for (let node in distances) {
+    let currentIsShortest = shortest === null || distances[node] < distances[shortest];
+    
+    if (currentIsShortest && !visited.includes(node)) {
+      shortest = node
+    }
+  }
+  
+  return shortest
 }
 
+// search shorth path in graph
+function dijkstra(startNode, endNode){
+  console.log(cities);
+}
