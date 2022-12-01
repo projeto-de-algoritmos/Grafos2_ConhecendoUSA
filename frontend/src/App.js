128441e4295ffb2868e3bcdfb6761fd791505e60
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2022-12-01 17:42:23 -0300

    feat: adding map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Home from './pages/Home';
 import './global/reset.global.css';
-import  'bootstrap/dist/css/bootstrap.min.css' ;
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, useMapEvents, Marker } from 'react-leaflet';
 import Form from 'react-bootstrap/Form';
+import mapIcon from '../../utils/mapIcon';
 import usaIcon from '../../assets/usa-icon3.png';
 import './index.css';
 
+
 const Home = () => {
+    const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+    const MyComponent = () => {
+        useMapEvents({
+            click: (event) => {
+                const { lat, lng } = event.latlng;
+                setPosition({
+                    latitude: lat,
+                    longitude: lng
+                });
+                console.log("latitude:" + lat, "longitude:" + lng);
+            }
+        });
+        return null;
+    }
+
+    const positions = [
+        {
+            latitude: 41.9596194,
+            longitude: -110.2286238
+        },
+        {
+            latitude: 45.8718012,
+            longitude: -104.8537176
+        },
+        {
+            latitude: 45.6138991,
+            longitude: -107.1917125
+        }
+    ]
+
     return (
         <div className="home-container">
             <div className="home-sidebar">
                 <div className="sidebar-title text-center">
-                    <h1>Conhecendo USA <img src={usaIcon}/></h1>
+                    <h1>Conhecendo USA <img src={usaIcon} /></h1>
                 </div>
                 <div className="sidebar-header">
                     <Form.Select aria-label="Default select example" className="mb-3">
-                        <option>Selecione o local de saída</option>
+                        <option>Selecione o ponto de partida</option>
                         <option value="1">One</option>
                         <option value="2">Two</option>
                         <option value="3">Three</option>
@@ -28,7 +59,7 @@ const Home = () => {
                     </Form.Select>
                 </div>
                 <div className="sidebar-results">
-                    
+
                 </div>
                 <div className="sidebar-footer">
                     <p>Victor Samuel dos Santos Lucas</p>
@@ -36,7 +67,35 @@ const Home = () => {
                 </div>
             </div>
             <div className="home-main">
-
+                <fildset>
+                    <MapContainer
+                        style={{ width: "100%", height: "100%" }}
+                        center={[40.3350942, -97.3138203]}
+                        zoom={5.3}
+                    >
+                        {/* <MyComponent /> */}
+                        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        {/* {
+                            positions && positions.map((p, index) =>
+                                <Marker
+                                    interactive={false}
+                                    icon={mapIcon}
+                                    position={[p.latitude, p.longitude]}
+                                />
+                            )
+                        } */}
+                        <MyComponent/>
+                        {
+                            position.latitude !== 0 && (
+                                <Marker
+                                    interactive={false}
+                                    icon={mapIcon}
+                                    position={[position.latitude, position.longitude]}
+                                />
+                            )
+                        }
+                    </MapContainer>
+                </fildset>
             </div>
         </div>
     )
