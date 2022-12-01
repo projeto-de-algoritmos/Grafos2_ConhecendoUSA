74dc31ca8d9abb9aca0e7e68b2ef0cfe46f6563d
faco400 <vd.souza1999@gmail.com>
2022-12-01 20:30:54 -0300

    add track of parents and distances to dijkstra algorithm

--- backend/src/graph.js
+++ backend/src/graph.js
@@ -17,5 +17,23 @@ let shortestDistanceNode = (distances, visited) => {
 
 // search shorth path in graph
 function dijkstra(startNode, endNode){
-  console.log(cities);
+  let distances = {}
+  distances[endNode] = "Infinity";
+  distances = Object.assign(distances, cities[startNode]);
+
+  let parents = {endNode: null};
+  for (i = 0; i < cities[startNode].edges.length; i++){
+    child = cities[startNode].edges[i]
+    // console.log(cities[startNode].edges);
+    parents[child] = cities[startNode].zipcode;
+  }
+  console.log(parents);
 }
+
+var testOrigin = cities.findIndex(p => p.zipcode == "98501");
+var testDest = cities.findIndex(p => p.zipcode == "87501");
+// origem = cities.indexOf(cities['zipcode'])
+dijkstra(testOrigin, testDest)
+// console.log(cities[0].indexOf(97301));
+// console.log(cities.indexOf('state': 'AL'))
+// console.log()
