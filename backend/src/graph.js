90bccda5e9388465a8ae5860d6c5e34c03824d56
faco400 <vd.souza1999@gmail.com>
2022-12-03 17:40:35 -0300

    finish dijkstra shortest path with working database

--- backend/src/graph.js
+++ backend/src/graph.js
@@ -1,39 +1,161 @@
-const {cities} = require('./database/states') 
+const {cities} = require('./database/states');
+const zipcodes = require('zipcodes');
 
-// search short distance between nodes
-let shortestDistanceNode = (distances, visited) => {
-  let shortest = null;
+function getNodeDistance(node1,node2) {
+  var dist = zipcodes.distance(node1, node2);
+  return zipcodes.toKilometers(dist);
+}
 
-  for (let node in distances) {
-    let currentIsShortest = shortest === null || distances[node] < distances[shortest];
+function init() { 
+
+  for(i = 0; i < cities.length; i++) {
     
-    if (currentIsShortest && !visited.includes(node)) {
-      shortest = node
+    let dists = {}
+    for(o = 0; o < cities[i].edges.length; o++){
+      
+      dists[cities[i].edges[o]] = getNodeDistance(cities[i].zipcode,cities[i].edges[o])
     }
+    cities[i]['dist'] = dists
   }
-  
-  return shortest
 }
 
-// search shorth path in graph
-function dijkstra(startNode, endNode){
-  let distances = {}
+function getNodeInformation(nodeZipcode) {
+  let nodePosition = cities.findIndex(p => p.zipcode == nodeZipcode);
+  return cities[nodePosition];
+}
+
+let shortestDistanceNode = (distances, visited) => {
+  // create a default value for shortest
+	let shortest = null;
+	
+  	// for each node in the distances object
+	for (let node in distances) {
+    	// if no node has been assigned to shortest yet
+  		// or if the current node's distance is smaller than the current shortest
+		let currentIsShortest =
+			shortest === null || distances[node] < distances[shortest];
+        	
+	  	// and if the current node is in the unvisited set
+		if (currentIsShortest && !visited.includes(node)) {
+            // update shortest to be the current node
+			shortest = node;
+		}
+	}
+	return shortest;
+};
+
+function findShortestPath(startNode, endNode) {
+
+  let distances = {};
+  // distances[endNode] = "Infinity";
+  let startPosition = cities.findIndex(p => p.zipcode == startNode);
+  distances = Object.assign(distances, cities[startPosition]).dist;
   distances[endNode] = "Infinity";
-  distances = Object.assign(distances, cities[startNode]);
+  // console.log(distances);
 
-  let parents = {endNode: null};
-  for (i = 0; i < cities[startNode].edges.length; i++){
-    child = cities[startNode].edges[i]
-    // console.log(cities[startNode].edges);
-    parents[child] = cities[startNode].zipcode;
+  let parents = {}
+  parents[endNode] = null;
+  for (i = 0; i < cities[startPosition].edges.length; i++){
+    child = cities[startPosition].edges[i]
+    // console.log(cities[startPosition].edges);
+    parents[child] = cities[startPosition].zipcode;
   }
-  console.log(parents);
+
+  visited = [];
+  let node = shortestDistanceNode(distances, visited);
+  // console.log(node);
+
+  while(node){
+    let distance = distances[node];
+    nodePos = cities.findIndex(p => p.zipcode == node)
+    let childrenZipcodes = cities[nodePos].edges
+    let childrenDist = [];
+    for(let i = 0; i < childrenZipcodes.length; i++){
+      childrenDist[i] = cities[nodePos].dist[childrenZipcodes[i]]
+    }
+    
+    for (childPos in childrenZipcodes){
+      // console.log(childPos);
+      zip = childrenZipcodes[childPos];
+      // console.log(childrenZipcodes);
+
+      if(zip === startNode){
+        continue;
+      }else{
+        let newdistance = distance + childrenDist[childPos];
+        // console.log(childrenDist[childPos]);
+
+        if(!distances[zip] || newdistance < distances[childrenZipcodes[childPos]]) {
+          distances[childrenZipcodes[childPos]] = newdistance;
+          // console.log(childPos);
+          // console.log(childrenZipcodes[childPos]);
+          // console.log(childrenDist[childPos]);
+          // console.log(newdistance);
+          parents[zip] = node;
+        }
+      }
+    }
+
+    visited.push(node);
+    node = shortestDistanceNode(distances,visited);
+    // console.log(node);
+
+  }
+  // console.log(distances);
+  // console.log(getNodeDistance('98501','97301'))
+  let shortestPath = [endNode];
+  let parent = parents[endNode];
+  let shortestPathNodes = [];
+  nodeInfo = zipcodes.lookup(endNode);
+
+  aux = {'distFromParent': null}
+  aux.distFromParent = distances[endNode] - getNodeDistance(parents[endNode],startNode);
+  // console.log(dist);
+
+  nodeInfo = Object.assign(nodeInfo,aux);
+  // console.log(nodeInfo);
+  shortestPathNodes.push(nodeInfo); 
+  // console.log(distances);
+  // console.log(parents);
+  // console.log(shortestPath);
+  // console.log(shortestPathNodes);
+  // console.log(parents[endNode]);
+
+  while(parent) {
+    shortestPath.push(parent);
+    nodeInfo = zipcodes.lookup(parent);
+    shortestPathNodes.push(nodeInfo);
+
+    aux = {'distFromParent': null}
+    aux.distFromParent = distances[parent] - getNodeDistance(parents[parent],startNode);
+
+    nodeInfo = Object.assign(nodeInfo,aux);
+
+    parent = parents[parent];
+  }
+  shortestPath.reverse();
+  shortestPathNodes.reverse();
+
+  result = {
+    totalDistance: distances[endNode],
+    path: shortestPath,
+    nodesInfo: shortestPathNodes
+  }
+
+  return result;
+  // console.log(distances[endNode]);
+  // console.log(shortestPath);
+
+  // console.log(getNodeDistance(98501,83701))
+  // console.log(getNodeDistance(83701,84101))
+  // console.log(getNodeDistance(84101,80201))
+
+
 }
 
-var testOrigin = cities.findIndex(p => p.zipcode == "98501");
-var testDest = cities.findIndex(p => p.zipcode == "87501");
-// origem = cities.indexOf(cities['zipcode'])
-dijkstra(testOrigin, testDest)
-// console.log(cities[0].indexOf(97301));
-// console.log(cities.indexOf('state': 'AL'))
-// console.log()
+// var testOrigin = cities.findIndex(p => p.zipcode == "98501");
+// var testDest = cities.findIndex(p => p.zipcode == "87501");
+init();
+// console.log(cities[0].dist['30301']);
+out = findShortestPath('98501', '87501');
+console.log(out);
