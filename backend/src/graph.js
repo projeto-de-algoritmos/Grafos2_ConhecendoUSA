1b5cd508869fa2070463eab7c66226394b6efebf
faco400 <vd.souza1999@gmail.com>
2022-12-05 13:16:48 -0300

    Update database and remove comments from graph.js

--- backend/src/graph.js
+++ backend/src/graph.js
@@ -48,23 +48,20 @@ function findShortestPath(startNode, endNode) {
   init();
 
   let distances = {};
-  // distances[endNode] = "Infinity";
   let startPosition = cities.findIndex(p => p.zipcode == startNode);
   distances = Object.assign(distances, cities[startPosition]).dist;
   distances[endNode] = "Infinity";
-  // console.log(distances);
+
 
   let parents = {}
   parents[endNode] = null;
   for (i = 0; i < cities[startPosition].edges.length; i++){
     child = cities[startPosition].edges[i]
-    // console.log(cities[startPosition].edges);
     parents[child] = cities[startPosition].zipcode;
   }
 
   visited = [];
   let node = shortestDistanceNode(distances, visited);
-  // console.log(node);
 
   while(node){
     let distance = distances[node];
@@ -76,22 +73,16 @@ function findShortestPath(startNode, endNode) {
     }
     
     for (childPos in childrenZipcodes){
-      // console.log(childPos);
       zip = childrenZipcodes[childPos];
-      // console.log(childrenZipcodes);
 
       if(zip === startNode){
         continue;
       }else{
         let newdistance = distance + childrenDist[childPos];
-        // console.log(childrenDist[childPos]);
 
         if(!distances[zip] || newdistance < distances[childrenZipcodes[childPos]]) {
           distances[childrenZipcodes[childPos]] = newdistance;
-          // console.log(childPos);
-          // console.log(childrenZipcodes[childPos]);
-          // console.log(childrenDist[childPos]);
-          // console.log(newdistance);
+
           parents[zip] = node;
         }
       }
@@ -99,11 +90,9 @@ function findShortestPath(startNode, endNode) {
 
     visited.push(node);
     node = shortestDistanceNode(distances,visited);
-    // console.log(node);
 
   }
-  // console.log(distances);
-  // console.log(getNodeDistance('98501','97301'))
+
   let shortestPath = [endNode];
   let parent = parents[endNode];
   let shortestPathNodes = [];
@@ -111,16 +100,9 @@ function findShortestPath(startNode, endNode) {
 
   aux = {'distFromParent': null}
   aux.distFromParent = distances[endNode] - getNodeDistance(parents[endNode],startNode);
-  // console.log(dist);
 
   nodeInfo = Object.assign(nodeInfo,aux);
-  // console.log(nodeInfo);
   shortestPathNodes.push(nodeInfo); 
-  // console.log(distances);
-  // console.log(parents);
-  // console.log(shortestPath);
-  // console.log(shortestPathNodes);
-  // console.log(parents[endNode]);
 
   while(parent) {
     shortestPath.push(parent);
@@ -144,23 +126,9 @@ function findShortestPath(startNode, endNode) {
   }
 
   return result;
-  // console.log(distances[endNode]);
-  // console.log(shortestPath);
-
-  // console.log(getNodeDistance(98501,83701))
-  // console.log(getNodeDistance(83701,84101))
-  // console.log(getNodeDistance(84101,80201))
-
 
 }
 
 module.exports = {
   findShortestPath
 }
-
-// var testOrigin = cities.findIndex(p => p.zipcode == "98501");
-// var testDest = cities.findIndex(p => p.zipcode == "87501");
-// init();
-// console.log(cities[0].dist['30301']);
-// out = findShortestPath('98501', '87501');
-// console.log(out);
